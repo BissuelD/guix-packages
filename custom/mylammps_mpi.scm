27fec4725053dc6548013a30b2bd063549f14524
Bissuel Dylan <dylan.bissuel@etu.umontpellier.fr>
2023-07-24 15:43:40 +0200

    Trying to solve ssh/rsh problem with guix MPI

--- custom/mylammps_mpi.scm
+++ custom/mylammps_mpi.scm
@@ -222,7 +222,7 @@
       ;;    > valgring ?
       ;;    > gdb ?
       ;;    > autossh
-      ("ssh", ssh)
+      ("mpich", mpich)
       ;; End of the additions ... for now ;) ;;
       ("n2p2-lib" ,n2p2-lib)          ; Because we don't build it manually on the fly, although I think
                                       ; it can easily be worked around. Not done because it is slower ?
