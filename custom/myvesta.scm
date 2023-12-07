2bc81210ff353b3027035356e8303bcb3e559b5b
BissuelD <dylan.bissuel@gmail.com>
2023-12-07 12:34:05 +0100

    Working with patchelf [in progress]

--- custom/myvesta.scm
+++ custom/myvesta.scm
@@ -1,15 +1,19 @@
 (define-module (myvesta)
     #:use-module ((guix licenses) #:prefix license:)
-    #:use-module (guix build-system copy)
     #:use-module (guix download)
     #:use-module (guix packages)
     #:use-module (guix profiles)
+    #:use-module (gnu packages base)
     #:use-module (gnu packages compression)
     #:use-module (gnu packages elf)
+    #:use-module (gnu packages fontutils)
     #:use-module (gnu packages gcc)
     #:use-module (gnu packages gl)
+    #:use-module (gnu packages glib)
     #:use-module (gnu packages gtk)
     #:use-module (gnu packages llvm)
+    #:use-module (gnu packages xorg)
+    #:use-module (nonguix build-system binary)
 )
 
 (define-public myvesta
@@ -24,38 +28,51 @@
           (sha256
           (base32
             "1y4dhqhk0jy7kbkkx2c6lsrm5lirn796mq67r5j1s7xkq8jz1gkq"))))
-  (build-system copy-build-system)
-  ; switch build system ?
-  ; include patchelf and/or patchelf-plan ?
+  (build-system binary-build-system)
+  ; switch build system ? -- OK (nonguix binary-buld-system)
+  ; include patchelf and/or patchelf-plan ? (included in ^)
   (arguments
-   `(#:phases 
-    (modify-phases %standard-phases
-      (add-after `unpack 'check-setup
-        (lambda* (#:key inputs #:allow-other-keys)
-          (setenv "DT_RUNPATH" (string-append (assoc-ref inputs "gcc") "/lib")))) ; this doesn't seem to work ? VARIABLE ??
-      ;(add-after 'unpack 'chmod
-	    ;  (lambda* (#:key #:allow-other-keys)
-	    ;    (chmod "VESTA" #o755)))
-      ;(delete `validate-runpath)
-    )
-    #:substitutable? #f
+   `(
+    #:strip-binaries? #f 
+    #:patchelf-plan 
+     (let ((libs '("cairo"
+                   ;"fontconfig"
+                   "gcc"
+                   "gdk-pixbuf"
+                   "glib"
+                   "glibc"
+                   "glu"
+                   "gtk+" 
+                   "libomp"
+                   "libx11"
+                   "libxxf86vm"
+                   "mesa"
+                   "pango")))
+     `(("libVESTA.so" ,libs)
+       ("VESTA"       ,libs)
+       ("VESTA-core"  ,libs)
+       ("VESTA-gui"   ,libs)
+       ("madel"       ,libs)))
+    ;#:validate-runpath? #f
     #:install-plan '(
       ("." "bin")
     )))
+  (native-inputs (list patchelf))
   (inputs
-    `(
-      ("gcc" ,gcc)
-      ("gtk+" ,gtk+)
-      ;("libomp" ,libomp)
-      ("mesa-utils" ,mesa-utils)
-    ))
-  (search-paths
-   (list (search-path-specification
-  ;        (variable "LD_LIBRARY_PATH")
-  ;        (files (list "lib" "lib/catalyst" "lib/mesa" "lib/mpi")))
-  ;        (search-path-specification
-          (variable "LD_LIBRARY_PATH")
-          (files (list "bin" (assoc-ref inputs "gcc"))))))
+   (list  cairo
+          ;fontconfig
+    (list gcc "lib")
+          gdk-pixbuf
+          glib
+          glibc
+          glu
+          gtk+
+          libomp
+          libx11
+          libxxf86vm
+          mesa
+          pango)
+  )
   (synopsis "VESTA - Visualization for Electronic and STructural Analysis.")
   (description "VESTA is a 3D visualization program for structural models, volumetric data such as electron/nuclear densities, and crystal morphologies.")
   (home-page "https://jp-minerals.org/vesta/en/")
